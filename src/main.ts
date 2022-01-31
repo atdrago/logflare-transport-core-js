3d611261ea9939f4f30c372f68a4cf533aa9aeae
Adam Drago <atdrago@gmail.com>
2022-01-31 12:06:25 -0500

    chore: Add LogflareHttpStreamClient export

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,2 @@
-import {LogflareHttpClient, LogflareUserOptionsI} from "./http_client"
-
-export {LogflareHttpClient, LogflareUserOptionsI}
+export {LogflareHttpClient, LogflareUserOptionsI} from "./http_client"
+export {LogflareHttpStreamClient} from "./http_stream_client"
