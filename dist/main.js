4c981173ecffbcfb48fc7fe268189055370c89dd
Adam Drago <atdrago@gmail.com>
2022-01-31 12:27:12 -0500

    chore: Rerun build

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.LogflareHttpClient = void 0;
+exports.LogflareHttpStreamClient = exports.LogflareHttpClient = void 0;
 var http_client_1 = require("./http_client");
 Object.defineProperty(exports, "LogflareHttpClient", { enumerable: true, get: function () { return http_client_1.LogflareHttpClient; } });
+var http_stream_client_1 = require("./http_stream_client");
+Object.defineProperty(exports, "LogflareHttpStreamClient", { enumerable: true, get: function () { return http_stream_client_1.LogflareHttpStreamClient; } });
 //# sourceMappingURL=main.js.map
